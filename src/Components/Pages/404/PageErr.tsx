4b77d944f869a386f50b52238c24f7dfa287571c
công nguyễn đức <duccongnguyen14102001@gmail.com>
2022-02-18 08:27:17 +0700

    create page 404

--- src/Components/Pages/404/PageErr.tsx
+++ src/Components/Pages/404/PageErr.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { RoutesString } from '../../Modules/routesString';
+import "./404.scss"
 
 const PageErr = () => {
   return (
-    <div>PageErr</div>
+    <div className="error">
+      <div className="error-container">
+        <h1>404</h1>
+        <p>Sorry, the page you visited does not exist.</p>
+        <Link to={RoutesString.Home}>Go Home</Link>
+      </div>
+    </div>
   )
 }
 
